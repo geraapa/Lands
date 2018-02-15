b3b5c884f8931f5333fe465c93e33645482cccf8
geraapa <geraapa@gmail.com>
2018-02-14 20:45:00 -0600

    Se completa models

---
+++ Lands/Lands/Models/RegionalBloc.cs
@@ -0,0 +1,22 @@
+﻿namespace Lands.Models
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class RegionalBloc
+    {
+        [JsonProperty(PropertyName = "acronym")]
+        public string Acronym { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "otherAcronyms")]
+        public List<object> OtherAcronyms { get; set; }
+
+        [JsonProperty(PropertyName = "otherNames")]
+        public List<object> OtherNames { get; set; }
+    }
+}
